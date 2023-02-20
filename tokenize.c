6f6185a74cb98821b7dd0251fde47e4675b8c0b4
Kris Amerman <amerman.k@northeastern.edu>
2023-02-20 10:54:24 -0500

    0

--- tokenize.c
+++ tokenize.c
@@ -132,18 +132,18 @@ vect_t *tokenize(char expr[]) {
 int main(int argc, char **argv) {
   
   char buffer[256];
-  int total_bytes = read(0, buffer, 255);  
-  buffer[total_bytes] = '\0';
-  close(0);
+  // int total_bytes = read(0, buffer, 255);  
+  // buffer[total_bytes] = '\0';
+  // close(0);
 
   // Takes buffer as expr
   vect_t *tokens = tokenize(buffer);
 
-  int i = 0;
-  while (i < tokens->size) {
-    printf("%s\n", tokens->data[i]);
-    ++i;
-  }
+  // int i = 0;
+  // while (i < tokens->size) {
+  //   printf("%s\n", tokens->data[i]);
+  //   ++i;
+  // }
 
   vect_delete(tokens);
 }
